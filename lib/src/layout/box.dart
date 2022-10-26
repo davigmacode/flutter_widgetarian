ad772475467d070c5c57828a43befc40449afbbf
Irfan Vigma Taufik <davigmacode@gmail.com>
2022-10-26 22:03:08 +0700

    Improve box widget

--- lib/src/layout/box.dart
+++ lib/src/layout/box.dart
@@ -4,71 +4,108 @@ class Box extends StatelessWidget {
   const Box({
     Key? key,
     required this.child,
-    required this.height,
+    this.width,
+    this.height,
+    this.padding,
+    this.margin,
     this.color,
     this.shape,
+    this.border,
+    this.borderSide,
+    this.borderRadius,
     this.shadowColor = const Color(0xFF000000),
     this.clipBehavior = Clip.antiAlias,
     this.elevation = 0.0,
   }) : super(key: key);
 
   final Widget child;
-  final double height;
+  final double? width;
+  final double? height;
+  final EdgeInsetsGeometry? padding;
+  final EdgeInsetsGeometry? margin;
   final Color? color;
   final Color shadowColor;
   final double elevation;
-  final ShapeBorder? shape;
+  final ShapeBorder? border;
+  final BorderSide? borderSide;
+  final BorderRadius? borderRadius;
+  final BoxShape? shape;
   final Clip clipBehavior;
+
+  ShapeBorder get defaultBorder {
+    return shape == BoxShape.circle
+        ? CircleBorder(
+            side: borderSide ?? BorderSide.none,
+          )
+        : RoundedRectangleBorder(
+            side: borderSide ?? BorderSide.none,
+            borderRadius: borderRadius ?? BorderRadius.zero,
+          );
+  }
 
   @override
   Widget build(BuildContext context) {
+    final border = this.border ?? defaultBorder;
     final textDirection = Directionality.maybeOf(context);
-    final borderClipper = shape == null
-        ? null
-        : ShapeBorderClipper(
-            textDirection: textDirection,
-            shape: shape!,
-          );
-
-    Widget contents = SizedBox(
-      height: height,
-      child: child,
+    final clipper = ShapeBorderClipper(
+      textDirection: textDirection,
+      shape: border,
     );
 
-    if (shape != null) {
-      contents = ShapeBorderPaint(
-        shape: shape!,
-        textDirection: textDirection,
+    Widget contents = child;
+
+    if (padding != null) {
+      contents = Padding(padding: padding!, child: contents);
+    }
+
+    if (width != null || height != null) {
+      contents = SizedBox(
+        width: width,
+        height: height,
         child: contents,
       );
     }
 
+    contents = ShapeBorderPaint(
+      textDirection: textDirection,
+      shape: border,
+      child: contents,
+    );
+
     if (color == null) {
-      return ClipPath(
-        clipper: borderClipper,
+      contents = ClipPath(
+        clipper: clipper,
         clipBehavior: clipBehavior,
         child: contents,
       );
     }
 
-    if (shape == null) {
-      return PhysicalModel(
+    if (shape != null) {
+      contents = PhysicalModel(
         color: color!,
         elevation: elevation,
         shadowColor: shadowColor,
         clipBehavior: clipBehavior,
+        borderRadius: borderRadius,
+        shape: shape!,
         child: contents,
       );
     }
 
-    return PhysicalShape(
+    contents = PhysicalShape(
       color: color!,
       elevation: elevation,
       shadowColor: shadowColor,
       clipBehavior: clipBehavior,
-      clipper: borderClipper!,
+      clipper: clipper,
       child: contents,
     );
+
+    if (margin != null) {
+      contents = Padding(padding: margin!, child: contents);
+    }
+
+    return contents;
   }
 }
 
