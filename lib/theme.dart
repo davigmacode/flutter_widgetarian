5be9bb7161d1856f0af10a4b0e5f55cae0773eb5
Irfan Vigma Taufik <davigmacode@gmail.com>
2022-11-18 17:33:11 +0700

    Update lib entry point

--- lib/theme.dart
+++ lib/theme.dart
@@ -2,7 +2,5 @@ library theme;
 
 export 'src/theme/main.dart';
 export 'src/theme/controller.dart';
-export 'src/theme/provider.dart';
-export 'src/theme/consumer.dart';
 export 'src/theme/model.dart';
 export 'src/theme/data.dart';
---
+++ lib/utils.dart
@@ -0,0 +1,5 @@
+library utils;
+
+export 'src/utils/colors.dart';
+export 'src/utils/lerp.dart';
+export 'src/utils/misc.dart';
